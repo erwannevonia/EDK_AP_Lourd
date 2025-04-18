16880cac70a470758875a30257543c4588b22f4a
erwannevonia <63651719+erwannevonia@users.noreply.github.com>
2025-04-18 15:42:26 +0200

    Update profil_screen.dart
    ui

--- lib/screens/profil_screen.dart
+++ lib/screens/profil_screen.dart
@@ -6,18 +6,36 @@ class ProfilScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      appBar: AppBar(
-        title: const Text('Mon Profil', style: TextStyle(color: Colors.white)),
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        elevation: 0,
-      ),
-      body: const Center(
-        child: Text(
-          'Page Profil en construction...',
-          style: TextStyle(color: Colors.black, fontSize: 18),
+        appBar: AppBar(
+          title:
+              const Text('Mon Profil', style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          elevation: 0,
         ),
-      ),
-    );
+        body: const Center(
+          child: Column(children: [
+            TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                labelText: 'Nom d\'utilisateur',
+                labelStyle: TextStyle(color: Colors.black),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                labelStyle: TextStyle(color: Colors.black),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+          ]),
+        ));
   }
 }
